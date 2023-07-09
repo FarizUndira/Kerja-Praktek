50bae8aee3efc5ea6c5c8ceee52c4146d547641a
FarizUndira <411201048@mahasiswa.undira.ac.id>
2023-07-09 16:10:56 +0700

    Update

--- resources/views/layouts/layout.blade.php
+++ resources/views/layouts/layout.blade.php
@@ -96,7 +96,7 @@
         <aside class="main-sidebar sidebar-dark-primary elevation-4">
             <!-- Brand Logo -->
             <a href="{{ url('/') }}" class="brand-link">
-                <span class="brand-text font-weight-light">UAS-FullStack</span>
+                <span class="brand-text font-weight-light">UAS-Web FullStack</span>
             </a>
 
             <!-- Sidebar -->
